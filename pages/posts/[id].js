86a7caddf79943b87e3f8277dc5f24398e44ec8c
Thiago Nobrega <thiagonobrega@MacBook-Air-de-Thiago.local>
2022-03-18 19:21:07 -0300

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import Date from "../../components/date";
+import Layout from "../../components/layout";
+import utilStyles from "../../styles/utils.module.css";
+import { getAllPostsIds, getPostData } from "../../lib/posts";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostsIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
